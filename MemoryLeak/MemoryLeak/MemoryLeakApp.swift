478431b177238fdfb331911ce3f12af36a991e3e
José Cruz <jose.garrido@g.globo>
2026-02-25 13:52:22 -0300

    create repository

---
+++ MemoryLeak/MemoryLeak/MemoryLeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLeakApp.swift
+//  MemoryLeak
+//
+//  Created by Jose Garrido on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
